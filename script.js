7865655e4bad4a2fe12919e5cbf92fbf42821b18
Angela Petreska <angelapetreska997@gmail.com>
2024-06-03 21:48:11 +0200

    final changes were made

--- script.js
+++ script.js
@@ -8,52 +8,51 @@ const pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-
 form.addEventListener('submit', e =>  {
     e.preventDefault();
 
-    let firstName =fname.value.trim();
-    let lastName =lname.value.trim();
-    let emailValue =email.value.trim();
-    let passwordValue =password.value.trim();
-    if(firstName === '') {
-        errorFunc(fname, 'First Name field cannot be empty' )
+    let firstName = fname.value.trim();
+    let lastName = lname.value.trim();
+    let emailValue = email.value.trim();
+    let passwordValue = password.value.trim();
+
+    if (firstName === '') {
+        errorFunc(fname, 'First Name field cannot be empty');
     } else {
-        successFunc(fname)
+        successFunc(fname);
     }
 
-    if(lastName === '') {
-        errorFunc(lname, 'Last Name field cannot be empty' )
+    if (lastName === '') {
+        errorFunc(lname, 'Last Name field cannot be empty');
     } else {
-        successFunc(lname)
+        successFunc(lname);
     }
 
-    if(emailValue === '') {
-        errorFunc(email, 'Email field cannot be empty' )
+    if (emailValue === '') {
+        errorFunc(email, 'Email field cannot be empty');
     } else if (!emailValue.match(pattern)) {
-        errorFunc(email, 'Looks like this is not an email')
-    }
-    else {
-        successFunc(email)
-    }
-
-    if(passwordValue === '') {
-        errorFunc(password, 'Password field cannot be empty' )
+        errorFunc(email, 'Looks like this is not an email');
     } else {
-        successFunc(password)
+        successFunc(email);
     }
 
-})
+    if (passwordValue === '') {
+        errorFunc(password, 'Password field cannot be empty');
+    } else {
+        successFunc(password);
+    }
+});
 
-function errorFunc(red, message) {
+function errorFunc(req, message) {
     const formControl = req.parentElement;
     const span = formControl.querySelector('span');
     span.innerText = message;
-    req.className += 'error';
-    span.className += 'error-text';
-    if( req !== email) {
+    req.classList.add('error');
+    span.classList.add('error-text');
+    if (req !== email) {
         req.value = '';
     } else {
-        req.styles.color ="hsl(0, 100%, 74%)";
+        req.style.color = "hsl(0, 100%, 74%)";
     }
 }
 
-function successFunction(req) {
-    req.className += 'success';
+function successFunc(req) {
+    req.classList.add('success');
 }
